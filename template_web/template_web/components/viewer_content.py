d17170b14e16d884a105c300412f4107faee3f74
jorgemonzonmarin <jmonzonmarin@gmail.com>
2025-06-13 17:27:26 +0200

    Añadidos botones de cambio de página en la parte superior de la tabla

--- template_web/template_web/components/viewer_content.py
+++ template_web/template_web/components/viewer_content.py
@@ -6,6 +6,20 @@ def viewer_content():
     return rx.box(
         rx.vstack(  
             rx.button("Ocultar/mostrar barra lateral", on_click=SidebarState.toggle_sidebar),
+            rx.hstack(
+                rx.button("⏮ First", on_click=DynamicTableState.first_page),
+                rx.button("⬅ Prev", on_click=DynamicTableState.prev_page),
+                rx.text(
+                    rx.cond(
+                        DynamicTableState.total_pages > 0,
+                        f"Page {DynamicTableState.page_number} of {DynamicTableState.total_pages}",
+                        "No data"
+                    )
+                ),
+                rx.button("Next ➡", on_click=DynamicTableState.next_page),
+                rx.button("Last ⏭", on_click=DynamicTableState.last_page),
+                spacing="2",
+            ),
             rx.box(
                 generate_dynamic_table(),
                 width="100%",
